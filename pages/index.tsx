d1fbccd9bd68f715d01fc2e544908ffb641e1df9
imjn <imajinkawabe@gmail.com>
2021-01-18 00:45:40 +0900

    fix: design

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link'
 import Layout from '../components/layout'
 import { getSorted3PostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
-import styles from '../styles/profile.module.scss'
+import profStyles from '../styles/profile.module.scss'
+import styles from '../styles/home.module.scss'
 import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
@@ -13,13 +14,13 @@ export default function Home({ allPostsData }) {
         <title>imjn | Portfolio & Blog</title>
       </Head>
       <div>
-        <div className={styles.profile}>
-          <img className={styles.profileImage} src="/images/profile.webp" alt=""/>
-          <h1 className={styles.title}>いまじん</h1>
+        <div className={profStyles.profile}>
+          <img className={profStyles.profileImage} src="/images/profile.webp" alt=""/>
+          <h1 className={profStyles.title}>いまじん</h1>
           <p>
             東京でソフトウェアエンジニアをしています。
           </p>
-          <div className={styles.socialIconsContainer}>
+          <div className={profStyles.socialIconsContainer}>
             <a target="_blank" href="https://twitter.com/mrimjn"><img src="/icons/twitter.svg" alt=""/></a>
             <a target="_blank" href="https://scrapbox.io/imjn"><img src="/icons/scrapbox.png" alt=""/></a>
           </div>
@@ -28,12 +29,12 @@ export default function Home({ allPostsData }) {
           </Link>
         </div>
         
-        <h2>Blog posts</h2>
+        <h2 className={styles.heading}>Blog posts</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
-                <a>{title}</a>
+                <a>{ title}</a>
               </Link>
               <br />
               <small className={utilStyles.lightText}>
@@ -43,8 +44,19 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
         <Link href="/posts">
-            <button className="button">More posts</button>
-          </Link>
+          <button className="button">More posts</button>
+        </Link>
+
+        <h2 className={`${styles.heading} ${styles.headingPortfolio}`}>Portfolios</h2>
+        <span className={`${utilStyles.lightText} ${styles.subHeading}`}>制作物の紹介です</span>
+        <img className={styles.portfolioImage} src="/icons/nishikigoi.webp" alt=""/>
+        <h3>
+          ニシキゴイ
+        </h3>
+        <p>ニシキゴイは写真俳句のSNSアプリです。これまでに70000句以上の投句がされています。初めてつくったアプリでした。</p>
+        <a target="_blank" href="https://apps.apple.com/jp/app/%E3%83%8B%E3%82%B7%E3%82%AD%E3%82%B4%E3%82%A4-%E4%BF%B3%E5%8F%A5%E3%81%A8%E5%86%99%E7%9C%9F%E3%81%AEsns/id1321285792?itsct=apps_box&itscg=30200">
+          AppStoreでみる
+        </a>
       </div>
     </Layout>
   )
